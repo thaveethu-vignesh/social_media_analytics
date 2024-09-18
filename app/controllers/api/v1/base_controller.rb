9e25b6780fc96424ae15b0536f7d18497d8faec1
thaveethu-vignesh <thaveethu.vignesh@freshworks.com>
2024-09-18 15:12:10 +0530

    Review changes - Explored cross cutting concerns
    Review  - Explored cross cutting concerns

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,16 +2,42 @@ module Api
   module V1
     class BaseController < ApplicationController
       protect_from_forgery with: :null_session
+      
       before_action :authenticate_request
-
+      before_action :set_request_start_time
+      after_action :log_request_details
+      around_action :catch_exceptions
+      
       rescue_from ActiveRecord::RecordNotFound, with: :not_found
       rescue_from ActionController::ParameterMissing, with: :bad_request
 
       private
 
       def authenticate_request
-        #can implement auth here
-        true
+        auth_token = params[:auth_token]
+        unless valid_auth_token?(auth_token)
+          render_error('Invalid or missing auth token', :unauthorized)
+        end
+      end
+
+      def valid_auth_token?(token)
+        token.present? && token == '12345'
+      end
+
+      def set_request_start_time
+        @request_start_time = Time.current
+      end
+
+      def log_request_details
+        duration = Time.current - @request_start_time
+        Rails.logger.info("Request to #{request.path} took #{duration} seconds")
+      end
+
+      def catch_exceptions
+        yield
+      rescue StandardError => e
+        Rails.logger.error("Error in #{controller_name}##{action_name}: #{e.message}")
+        render_error('An unexpected error occurred', :internal_server_error)
       end
 
       def not_found

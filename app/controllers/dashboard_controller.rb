e8947b9ed0c2f328ff97e5120f2802d2f7f80c6e
thaveethu-vignesh <thaveethu.vignesh@freshworks.com>
2024-09-18 18:26:22 +0530

    Removed un used variables
    Removed un used variables

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,12 @@ class DashboardController < ApplicationController
     @user_count = User.count
     @post_count = Post.count
     @interaction_count = Interaction.count
-    @last_generation = DataGenerationWorker.perform_in(59.minute, 1, 5, 10) 
+    #@last_generation = DataGenerationWorker.perform_in(59.minute, 1, 5, 10) 
   end
 
   def data_generate
     # Trigger a data generation task here
-   # DataGenerationWorker.perform_async(10, 50, 100)
+   DataGenerationWorker.perform_async(10, 50, 100)
     redirect_to dashboard_index_path, notice: 'Data generation has been triggered!'
   end
 
